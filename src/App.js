2d33cf673297334e4b569a753867d56505106a9f
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-07-23 16:05:20 -0600

    New arrays for each Nav button

--- src/App.js
+++ src/App.js
@@ -14,15 +14,53 @@ class App extends Component {
     this.state = {
       photos: [],
       loading: true,
+      cats: [],
+      dogs: [],
+      computers: []
     }
   }
 
 
   componentDidMount () {
     this.performSearch();
+
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => {
+        this.setState({
+          cats: res.data.photos.photo,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      })
+
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => {
+        this.setState({
+          dogs: res.data.photos.photo,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      })
+
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=computers&per_page=24&format=json&nojsoncallback=1`)
+      .then(res => {
+        this.setState({
+          computers: res.data.photos.photo,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      })
   }
 
   performSearch = (query = 'cats') => {
+    this.setState({ loading: true });
+
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(res => {
         this.setState({
@@ -42,14 +80,18 @@ class App extends Component {
         <div className='container'>
           <SearchForm onSearch={ this.performSearch }/>
           <Nav />
+          {
+          (this.state.loading) 
+          ? <p>Loading... </p>
+          :
           <Switch>
             <Route exact path='/' render={ () => <Redirect to='/cats'/>} />
-            <Route path="/cats" render={ () => (this.state.loading) ? <p>Loading... </p>: <PhotoContainer data={ this.state.photos } />} />
-            <Route path='/dogs' component={NotFound} />
-            <Route path='/computers' component={NotFound} />
-            <Route path='/:searchText' component={NotFound} />
+            <Route path="/cats" render={ () =>  <PhotoContainer data={ this.state.cats } />} />
+            <Route path='/dogs' render={ () =>  <PhotoContainer data={ this.state.dogs } />} />
+            <Route path='/computers' render={ () =>  <PhotoContainer data={ this.state.computers } />} />
             <Route component={NotFound} />
           </Switch>
+          }
         </div>
       </BrowserRouter>
     );
